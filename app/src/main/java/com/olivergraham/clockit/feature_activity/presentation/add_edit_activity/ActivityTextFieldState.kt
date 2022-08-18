d12ce22b141fbbc1afa3e91995b2ef472d4d9739
Oliver <grahamol@oregonstate.edu>
2022-08-18 16:33:57 -0700

    Text field state

---
+++ app/src/main/java/com/olivergraham/clockit/feature_activity/presentation/add_edit_activity/ActivityTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.olivergraham.clockit.feature_activity.presentation.add_edit_activity
+
+data class ActivityTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
