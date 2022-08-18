86177aa8346e2ce0fbbc6d7c992641c4192f38bd
Oliver <grahamol@oregonstate.edu>
2022-08-18 16:33:39 -0700

    Sealed class of events for save screen

---
+++ app/src/main/java/com/olivergraham/clockit/feature_activity/presentation/add_edit_activity/AddEditActivityEvent.kt
@@ -0,0 +1,12 @@
+package com.olivergraham.clockit.feature_activity.presentation.add_edit_activity
+
+import androidx.compose.ui.focus.FocusState
+
+sealed class AddEditActivityEvent{
+    data class EnteredTitle(val value: String): AddEditActivityEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): AddEditActivityEvent()
+    data class EnteredContent(val value: String): AddEditActivityEvent()
+    data class ChangeContentFocus(val focusState: FocusState): AddEditActivityEvent()
+    data class ChangeColor(val color: Int): AddEditActivityEvent()
+    object SaveActivity: AddEditActivityEvent()
+}
